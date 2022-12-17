52da9ce3be15c4efd5ac3ea66d7d50d9cf06e94b
rouhan14 <rfaisal.bscs21seecs@seecs.edu.pk>
2022-12-18 00:26:44 +0500

    Lexicon is now made with Forward Index

--- Task 2/task2.py
+++ Task 2/task2.py
@@ -1,169 +1,3 @@
-# import nltk
-# import json
-
-# from nltk.corpus import stopwords
-# stop_words = stopwords.words('english')
-
-# # nltk.data.path.append()
-
-# import panda as pd
-
-# import json
-# file = open('D:\\Sem Projects\\DSA Project\\General Shit\\practise\\dir\\911truthorg.json', 'r')
-
-
-# my_data = json.load(file)
-# print(my_data)
-# print(type(my_data))
-
-
-# file.close()
-
-# This removes the stop words from the sentence
-# from nltk.corpus import stopwords
-# from nltk.tokenize import word_tokenize
-
-# example_text = "This is an exapmmle showing off stop word filtration."
-
-# stop_words = set(stopwords.words("english"))
-
-# words = word_tokenize(example_text)
-
-# filtered_sentence = []
-
-# for w in words:
-#     if w not in stop_words:
-#         filtered_sentence.append(w)
-
-# print(filtered_sentence)task2.py
-
-# Stemming
-# from nltk.stem import PorterStemmer
-# from nltk.tokenize import word_tokenize
-
-# ps = PorterStemmer()
-
-# # exapmle_words = ["python", "pyhtoner", "pythoning", "pythoned", "pythonly"]
-
-# # for w in exapmle_words:
-# #     print(ps.stem(w))
-
-
-# new_text = "It is very important to be pythonly while pythoning with python. All pythoners have pythoned poorly."
-
-# words = word_tokenize(new_text)
-
-
-# for w in words:
-#     print(ps.stem(w))
-
-
-# Regex Removing all special characters
-# import re
-# re.sub(r'[^\w\s]', "", i)
-
-
-# Lemmatizing
-# from nltk.stem import WordNetLemmatizer
-
-# lemmatizer = WordNetLemmatizer()
-
-# print(lemmatizer.lemmatize('cats'))
-# print(lemmatizer.lemmatize("better", pos="a"))
-# print(lemmatizer.lemmatize("best", pos="a"))
-# print(lemmatizer.lemmatize("run", pos="v"))
-# print(lemmatizer.lemmatize('cacti'))
-# print(lemmatizer.lemmatize('geese'))
-# print(lemmatizer.lemmatize('rocks'))
-# print(lemmatizer.lemmatize('python'))
-
-
-# now writing the original code
-# Making a Lexicon
-
-# import os
-# import json
-# import re
-# from nltk.corpus import stopwords
-# from nltk.tokenize import word_tokenize, sent_tokenize
-# from nltk.corpus import stopwords
-# from nltk.stem import PorterStemmer
-
-# FOLDER_PATH = r'D:\\Sem Projects\\DSA Project\\General Shit\\practise\\dir'
-
-# filenames = os.listdir(FOLDER_PATH)
-
-# ps = PorterStemmer()
-
-# stop_words = set(stopwords.words("english"))
-
-# json_file = open(f'D:\\Sem Projects\\DSA Project\\General Shit\\practise\\dir\\{filenames[0]}', 'r')
-
-# data = json.load(json_file)
-
-
-# def make_dict_object(id, key):
-#     new_dict = {'id': id, 'key': key, 'count': 1}
-#     return new_dict
-
-
-# lexicon = {}
-# hits = {}
-
-# lex_id_generator = 0
-
-# id_generator = 0
-# for dic in data:
-#     sent_title = dic['title'].lower()
-#     sent_content = dic['content'].lower()
-#     tokenize_title = word_tokenize(sent_title)
-#     tokenize_content = word_tokenize(sent_content)
-#     tokenize_title = [i for i in tokenize_title if re.sub(r'[^\w\s]', "", i)]
-#     tokenize_content = [i for i in tokenize_title if re.sub(r'[^\w\s]', "", i)]
-
-#     for i in range(len(tokenize_title)):
-#         tokenize_title[i] = tokenize_title[i].lower()
-#         if tokenize_title[i] not in stop_words:
-#             ps.stem(tokenize_title[i])
-#             lexicon[tokenize_title[i]] = lex_id_generator
-#             lex_id_generator += 1
-#             # if tokenize_title[i] in lexicon.keys():
-#             #     lexicon[tokenize_title[i]]['count'] += 1
-#             # else:
-#             #     word_obj = make_dict_object(id_generator, 'title')
-#             #     id_generator += 1
-#             #     lexicon[tokenize_title[i]] = word_obj
-
-#     for i in range(len(tokenize_content)):
-#         tokenize_content[i] = tokenize_content[i].lower()
-#         if tokenize_content[i] not in stop_words:
-#             ps.stem(tokenize_content[i])
-#             lexicon[tokenize_content[i]] = lex_id_generator
-#             lex_id_generator += 1
-#             # if tokenize_content[i] in lexicon.keys():
-#             #     lexicon[tokenize_content[i]]['count'] += 1
-#             # else:
-#             #     word_obj = make_dict_object(id_generator, 'content')
-#             #     id_generator += 1
-#             #     lexicon[tokenize_content[i]] = word_obj
-
-
-
-#     # for word in tokenize_words:
-#     #     word = word.lower()
-#     #     if word not in stop_words:
-#     #         ps.stem(word)
-#     #         word_obj = make_dict_object(id_generator, word, 'title')
-#     #         id_generator += 1
-#     #         lexicon.append(word_obj)
-
-# json_file.close()
-
-# with open("lexicon.json", "w") as outfile:
-#     json.dump(lexicon, outfile)
-
-
-
 # Agar to key alreadey persent ha to count ki value bharao warna already hua procedure repeat karwao
 # Pehla set of data ko dictionary bnao....usko json bnao....usko phir dict bnao....usko phir dict bnao....wtf lkn isse to kuch badly ga hi nahi
 
@@ -171,23 +5,39 @@
 # Total articles being crawled through right now: 15365
 # Lexicon Creator
 import os
+import time
 import json
 import re
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
+from nltk.stem.wordnet import WordNetLemmatizer
 FOLDER_PATH = r'D:\\Sem Projects\\DSA Project\\General Shit\\practise\\dir'
+
+
+def wordID_maker(hit, string):
+    wordID = {'pos': [], 'hits': hit, 'type': string}
+    return wordID
+
 
 filenames = os.listdir(FOLDER_PATH)
 
 ps = PorterStemmer()
 
+wordnet = WordNetLemmatizer()
+
 stop_words = set(stopwords.words("english"))
 
 lexicon = {}
 
+forward_index = {}
+
 lex_id_generator = 0
+
+doc_id = 0
+
+start = time.time()
 
 for i in range(len(filenames)):
     json_file = open(f'D:\\Sem Projects\\DSA Project\\General Shit\\practise\\dir\\{filenames[i]}', 'r')
@@ -202,26 +52,78 @@ for i in range(len(filenames)):
         tokenize_title = [i for i in tokenize_title if re.sub(r'[^\w\s]', "", i)]
         tokenize_content = [i for i in tokenize_content if re.sub(r'[^\w\s]', "", i)]
 
+
+        tokenize_title_less_stop_words = []
+        tokenize_content_less_stop_words = []
+
+        doc_key = {f'{doc_id}': {}}
+
+
+
         for i in range(len(tokenize_title)):
-            # tokenize_title[i] = tokenize_title[i].lower()
             if tokenize_title[i] not in stop_words:
-                ps.stem(tokenize_title[i])
+                wordnet.lemmatize(ps.stem(tokenize_title[i]))
+                tokenize_title_less_stop_words.append(tokenize_title[i])
                 if tokenize_title[i] not in lexicon.keys():
                     lexicon[tokenize_title[i]] = lex_id_generator
                     lex_id_generator += 1
 
+        for i in range(len(tokenize_title_less_stop_words)):
+            if tokenize_title_less_stop_words[i] in lexicon.keys() and str(lexicon[f'{tokenize_title_less_stop_words[i]}']) not in doc_key[f'{doc_id}'].keys():
+                doc_key[f'{doc_id}'].update({str(lexicon[f'{tokenize_title_less_stop_words[i]}']): wordID_maker(0, 'title')})
+
+            if str(lexicon[f'{tokenize_title_less_stop_words[i]}']) in doc_key[f'{doc_id}'].keys():
+                temp = str(lexicon[f'{tokenize_title_less_stop_words[i]}'])
+                val = doc_key[f'{doc_id}'][str(temp)]['hits'] + 1
+                doc_key[f'{doc_id}'][str(temp)]['hits'] = val
+
+            temp2 = str(lexicon[f'{tokenize_title_less_stop_words[i]}'])
+            if str(lexicon[f'{tokenize_title_less_stop_words[i]}']) in doc_key[f'{doc_id}'].keys() and doc_key[f'{doc_id}'][str(temp2)]['type'] == 'title':
+                doc_key[f'{doc_id}'][str(temp)]['pos'].append(i)
+
         for i in range(len(tokenize_content)):
-            # tokenize_content[i] = tokenize_content[i].lower()
             if tokenize_content[i] not in stop_words:
-                ps.stem(tokenize_content[i])
+                wordnet.lemmatize(ps.stem(tokenize_content[i]))
+                tokenize_content_less_stop_words.append(tokenize_content[i])
                 if tokenize_content[i] not in lexicon.keys():
                     lexicon[tokenize_content[i]] = lex_id_generator
                     lex_id_generator += 1
 
+        for i in range(len(tokenize_content_less_stop_words)):
+            if tokenize_content_less_stop_words[i] in lexicon.keys() and str(lexicon[f'{tokenize_content_less_stop_words[i]}']) in doc_key[f'{doc_id}'].keys() and doc_key[f'{doc_id}'][str(temp)]['type'] == 'title':
+                doc_key[f'{doc_id}'][str(temp)]['type'] = 'both'
+
+            if tokenize_content_less_stop_words[i] in lexicon.keys() and str(lexicon[f'{tokenize_content_less_stop_words[i]}']) not in doc_key[f'{doc_id}'].keys():
+                doc_key[f'{doc_id}'].update(
+                    {str(lexicon[f'{tokenize_content_less_stop_words[i]}']): wordID_maker(0, 'content')})
+
+            if str(lexicon[f'{tokenize_content_less_stop_words[i]}']) in doc_key[f'{doc_id}'].keys():
+                temp = str(lexicon[f'{tokenize_content_less_stop_words[i]}'])
+                val = doc_key[f'{doc_id}'][str(temp)]['hits'] + 1
+                doc_key[f'{doc_id}'][str(temp)]['hits'] = val
+
+            temp2 = str(lexicon[f'{tokenize_content_less_stop_words[i]}'])
+            if str(lexicon[f'{tokenize_content_less_stop_words[i]}']) in doc_key[f'{doc_id}'].keys() and doc_key[f'{doc_id}'][str(temp2)]['type'] == 'content' or 'both':
+                doc_key[f'{doc_id}'][str(temp)]['pos'].append(i)
+
+        doc_id += 1
+        forward_index.update(doc_key)
+
     json_file.close()
+
 
 with open('lexicon.json', 'w') as outfile:
     json.dump(lexicon, outfile)
+
+with open ('forwardIndex.json', 'w') as outfile:
+    json.dump(forward_index, outfile)
+
+
+end = time.time()
+
+print(end - start)
+
+
 
 
 # New Concept:
--- Task 4/test.py
+++
@@ -1,6 +0,0 @@
-dic1 = {'wordID': {'docID': {'hits': 123}, 'pos': [1,2,3,4,5,6]}}
-dic2 = {'doc': {'hits': 12, 'pos': [7,6,5,4,3]}}
-
-dic1['wordID'].update(dic2)
-
-print(dic1)
