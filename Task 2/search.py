79b9cdab7c93f00731c50059be75fe5fb7552663
rouhan14 <rfaisal.bscs21seecs@seecs.edu.pk>
2022-12-29 00:48:20 +0500

    added multi word query

--- Task 2/search.py
+++ Task 2/search.py
@@ -12,32 +12,30 @@ inverted_data = json.load(inverted_file)
 inverted_file.close()
 
 # Doing one word query right now
-word = input('Enter the word to be searched: ')
+sentence = input('Search: ')
+
+word_arr = sentence.split()
 
 list_to_be_disp = []
 weight = []
 
-if word in lexicon_data:
-    dic_found = inverted_data[str(lexicon_data[word])]
-    for key in dic_found.keys():
-        list_to_be_disp.append(key)
-        weight.append([int(key), 0])
+dic_found = {}
 
-else:
-    print('No such word found')
+for word in word_arr:
 
-# Weightage dictionary
+    if word in lexicon_data and word not in dic_found:
+        dic_found = inverted_data[str(lexicon_data[word])]
+        for key in dic_found.keys():
+            list_to_be_disp.append(key)
+            weight.append([int(key), 0])
+    else:
+        print('No such word found')
 
-title_hit = 0
-content_hit = 0
-
-print(dic_found)
 print(list_to_be_disp)
-print(weight)
+
 
 if (len(list_to_be_disp) != 0):
     for wordID in range(len(list_to_be_disp)):
-        print(dic_found[list_to_be_disp[wordID]])
         if 'title' in dic_found[list_to_be_disp[wordID]]:
             temp = dic_found[list_to_be_disp[wordID]]['title']
             weight[wordID][1] = 10*temp
@@ -46,9 +44,26 @@ if (len(list_to_be_disp) != 0):
             temp = dic_found[list_to_be_disp[wordID]]['content']
             weight[wordID][1] += 5*temp
 
-weight.sort(key=lambda x: x[1])
+# create an empty dictionary to store the merged lists
+merged_dict = {}
 
-print(weight)
+# iterate through the input list
+for lst in weight:
+    # if the first element of the list is already in the dictionary
+    if lst[0] in merged_dict:
+        # add the second element of the list to the value of the first element
+        merged_dict[lst[0]] += lst[1]
+    # if the first element of the list is not in the dictionary
+    else:
+        # add the first element as the key and the second element as the value
+        merged_dict[lst[0]] = lst[1]
+
+# convert the dictionary to a list of lists
+output_weight = [[k, v] for k, v in merged_dict.items()]
+
+# print the output list
+
+output_weight.sort(key=lambda x: x[1], reverse=True)
 
 doc_index_file = open(
     f'D:\\Sem Projects\\DSA Project\\General Shit\\Suche\\Task 2\\docIndex.json')
@@ -57,7 +72,8 @@ doc_index = json.load(doc_index_file)
 doc_index_file.close()
 
 count = 0
-for doc_arr in weight:
-    if count == 15:
-        break
-    print(doc_index[str(doc_arr[0])])
+if (len(output_weight) != 0):
+    for doc_arr in output_weight:
+        if count == 15:
+            break
+        print(doc_index[str(doc_arr[0])])
--- Task 2/test.py
+++ Task 2/test.py
@@ -1,29 +1,35 @@
-import os
-import json
-from nltk import PorterStemmer, WordNetLemmatizer, pos_tag
+# import os
+# import json
+# from nltk import PorterStemmer, WordNetLemmatizer, pos_tag
 
-text = """Welcome you to programming knowledge. Lets start with our first tutorial on NLTK. We shell learn bascis of NLTK here."""
-demoWords = ['playing', 'happiness', 'going', 'doing', 'yes', 'no', 'I', 'having', 'had', 'haved', 'coding', 'programming', 'code', 'program', 'playing']
+# text = """Welcome you to programming knowledge. Lets start with our first tutorial on NLTK. We shell learn bascis of NLTK here."""
+# demoWords = ['playing', 'happiness', 'going', 'doing', 'yes', 'no', 'I', 'having', 'had', 'haved', 'coding', 'programming', 'code', 'program', 'playing']
 
-lematizer = WordNetLemmatizer()
-ps = PorterStemmer()
+# lematizer = WordNetLemmatizer()
+# ps = PorterStemmer()
 
-lemaList = ['a', 's', 'r', 'n', 'v']
+# lemaList = ['a', 's', 'r', 'n', 'v']
 
-# print([i for i in lemaList])
+# # print([i for i in lemaList])
 
-dic1 = {"name": {"Rouhan": {"type": 'title'}, "Rouhan": {"type": 'content'}}}
+# dic1 = {"name": {"Rouhan": {"type": 'title'}, "Rouhan": {"type": 'content'}}}
 
-# temp = {"Rouhan" : {'type': 'content'}}
+# # temp = {"Rouhan" : {'type': 'content'}}
 
-# dic1["name"].update(temp)
+# # dic1["name"].update(temp)
 
-val = 0
-if dic1['name']['Rouhan'].keys() == 'type':
-    print('dsa')
+# val = 0
+# if dic1['name']['Rouhan'].keys() == 'type':
+#     print('dsa')
 
 # print()
 
 # for word in demoWords:
     # Left side is the stemmer and the right side is lemmatizer.
     # print(word, ps.stem(word), lematizer.lemmatize(word, pos=))
+
+
+list1 = [1,2,3,4,5,2,3,5,'rouhan']
+
+if 10 not in list1:
+    print('dsa')
